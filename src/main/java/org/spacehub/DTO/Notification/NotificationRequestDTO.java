df17f68ff4ff9db82bb73ffbe2c68372a781e261
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-25 17:14:10 +0530

    Logic improved of Friend Service class

--- src/main/java/org/spacehub/DTO/Notification/NotificationRequestDTO.java
+++ src/main/java/org/spacehub/DTO/Notification/NotificationRequestDTO.java
@@ -16,5 +16,5 @@ public class NotificationRequestDTO {
     private Long communityId;
     private Long referenceId;
     private String scope;
-    private boolean actionable;
+    private Boolean actionable;
 }
