331cc12f9be8db00d905b365107ec9b65120e99d
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-25 20:15:06 +0530

    profile image upload feature added

--- src/main/java/org/spacehub/controller/DashboardController.java
+++ src/main/java/org/spacehub/controller/DashboardController.java
@@ -1,13 +1,18 @@
 package org.spacehub.controller;
 
+import jakarta.validation.Valid;
 import org.spacehub.DTO.DashBoard.UsernameRequest;
-import org.spacehub.entities.User.User;
+import org.spacehub.entities.ApiResponse.ApiResponse;
 import org.spacehub.service.DashboardService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/dashboard")
+@RequestMapping("/api/v1/dashboard")
 public class DashboardController {
 
     private final DashboardService dashboardService;
@@ -17,11 +22,18 @@ public class DashboardController {
     }
 
     @PostMapping("/set-username")
-    public ResponseEntity<User> setUsername(@RequestBody UsernameRequest request) {
-        User updatedUser = dashboardService.saveUsernameByEmail(
+    public ResponseEntity<ApiResponse<String>> setUsername(@Valid @RequestBody UsernameRequest request) {
+        ApiResponse<String> resp = dashboardService.saveUsernameByEmail(
                 request.getEmail(),
                 request.getUsername()
         );
-        return ResponseEntity.ok(updatedUser);
+        return ResponseEntity.status(resp.getStatus()).body(resp);
     }
+
+    @PostMapping(value = "/upload-profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<String>> uploadProfileImage(@RequestParam("email") String email, @RequestParam("image") MultipartFile image) {
+        ApiResponse<String> response = dashboardService.uploadProfileImage(email, image);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
 }
