01e24af34a966a51df5bd46ca5121a86ce5caaca
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-30 08:47:09 +0530

    Cors credentials allowed

--- src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
+++ src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
@@ -18,6 +18,6 @@ public class WebSocketConfigJanus implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5500", "http://127.0.0.1:5500").withSockJS();
+    registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5500", "https://codewithketan.me").withSockJS();
   }
 }
