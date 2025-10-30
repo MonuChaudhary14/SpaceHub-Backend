d1226b2d1e3a4ab12a7807921a67edaa34c4cde9
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-30 21:42:47 +0530

    Updated cors to allow localhost 5173

--- src/main/java/org/spacehub/configuration/SecurityConfiguration.java
+++ src/main/java/org/spacehub/configuration/SecurityConfiguration.java
@@ -35,7 +35,10 @@ public class SecurityConfiguration {
     config.setAllowedOrigins(List.of(
             "http://127.0.0.1:5500",
             "http://localhost:5500",
-            "https://codewithketan.me"
+            "https://codewithketan.me",
+            "https://localhost:5173",
+            "https://space-hub-frontend.vercel.app",
+            "https://www.spacehubx.me"
     ));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("*"));
