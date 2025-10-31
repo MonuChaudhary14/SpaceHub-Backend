917892a6da7e808fe0108a54a6ea03fb0e7397c9
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-31 13:54:28 +0530

    Cors changed

--- src/main/java/org/spacehub/configuration/SecurityConfiguration.java
+++ src/main/java/org/spacehub/configuration/SecurityConfiguration.java
@@ -32,10 +32,21 @@ public class SecurityConfiguration {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
 
-    config.setAllowedOriginPatterns(List.of("*"));
+    config.setAllowedOrigins(List.of(
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "http://localhost:5173",
+            "http://localhost:8080",
+            "https://codewithketan.me",
+            "https://space-hub-frontend.vercel.app",
+            "https://www.spacehubx.me",
+            "https://audio-room-tawny.vercel.app"
+    ));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("*"));
     config.setExposedHeaders(List.of("Authorization"));
+    config.setAllowCredentials(true);
+    config.setMaxAge(3600L);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", config);
     return source;
