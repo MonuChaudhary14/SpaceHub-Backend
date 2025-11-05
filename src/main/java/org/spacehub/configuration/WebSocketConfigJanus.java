28e9bfec98212907c86536131d52cae9f15bf22a
somiljain2006 <somil16022006@gmail.com>
2025-11-06 02:26:09 +0530

    Minor change in web socket config

--- src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
+++ src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
@@ -24,6 +24,11 @@ public class WebSocketConfigJanus implements WebSocketMessageBrokerConfigurer {
 //            .setAllowedOrigins("https://codewithketan.me", "http://127.0.0.1:5500", "http://localhost:5173",
 //              "http://localhost:8080", "https://space-hub-frontend.vercel.app", "https://www.spacehubx.me",
 //              "https://audio-room-tawny.vercel.app", "http://127.0.0.1:5500/coding/index.html",
-//              "https://somiljain2006.github.io/Audio-room", "https://somiljain2006.github.io").withSockJS();
+//              "https://somiljain2006.github.io/Audio-room/", "https://somiljain2006.github.io/").withSockJS();
+//            .setAllowedOrigins("https://codewithketan.me", "http://127.0.0.1:5500", "http://localhost:5173",
+//              "http://localhost:8080", "https://space-hub-frontend.vercel.app", "https://www.spacehubx.me",
+//              "https://audio-room-tawny.vercel.app", "http://127.0.0.1:5500/coding/index.html",
+//              "https://somiljain2006.github.io/Audio-room/",
+//              "https://audio-room-tawny.vercel.app", "https://audio-room-nine.vercel.app").withSockJS();
   }
 }
