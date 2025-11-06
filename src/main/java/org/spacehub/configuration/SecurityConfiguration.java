b5f4f3151994433dd173b49bbe152ac43c3eb714
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-11-06 13:43:57 +0530

    Audio room updated

--- src/main/java/org/spacehub/configuration/SecurityConfiguration.java
+++ src/main/java/org/spacehub/configuration/SecurityConfiguration.java
@@ -32,19 +32,19 @@ public class SecurityConfiguration {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
 
-    config.setAllowedOriginPatterns(List.of("*"));
+//    config.setAllowedOriginPatterns(List.of("*"));
 
-//    config.setAllowedOrigins(List.of(
-//      "http://127.0.0.1:5500",
-//      "http://localhost:5500",
-//      "http://localhost:5173",
-//      "http://localhost:8080",
-//      "https://codewithketan.me",
-//      "https://space-hub-frontend.vercel.app",
-//      "https://www.spacehubx.me",
-//      "https://audio-room-tawny.vercel.app",
-//      "http://127.0.0.1:5500/coding/index.html"
-//    ));
+    config.setAllowedOrigins(List.of(
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "http://localhost:5173",
+      "http://localhost:8080",
+      "https://codewithketan.me",
+      "https://space-hub-frontend.vercel.app",
+      "https://www.spacehubx.me",
+      "https://audio-room-tawny.vercel.app",
+      "https://audio-room-nine.vercel.app/"
+    ));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("*"));
     config.setExposedHeaders(List.of("Authorization"));
--- src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
+++ src/main/java/org/spacehub/configuration/WebSocketConfigJanus.java
@@ -19,11 +19,11 @@ public class WebSocketConfigJanus implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws")
-      .setAllowedOriginPatterns("*")
-      .withSockJS();
-//            .setAllowedOrigins("https://codewithketan.me", "http://127.0.0.1:5500", "http://localhost:5173",
-//              "http://localhost:8080", "https://space-hub-frontend.vercel.app", "https://www.spacehubx.me",
-//              "https://audio-room-tawny.vercel.app", "http://127.0.0.1:5500/coding/index.html",
-//              "https://somiljain2006.github.io/Audio-room/", "https://somiljain2006.github.io/").withSockJS();
+//      .setAllowedOriginPatterns("*")
+//      .withSockJS();
+            .setAllowedOrigins("https://codewithketan.me", "http://127.0.0.1:5500", "http://localhost:5173",
+              "http://localhost:8080", "https://space-hub-frontend.vercel.app", "https://www.spacehubx.me",
+              "https://audio-room-tawny.vercel.app", "https://audio-room-nine.vercel.app",
+              "https://somiljain2006.github.io/Audio-room/", "https://somiljain2006.github.io/").withSockJS();
   }
 }
