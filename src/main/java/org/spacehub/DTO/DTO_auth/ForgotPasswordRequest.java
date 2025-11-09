6ecdf0e3ed98b6cf7d4d1f39eb487c72077c924c
somiljain2006 <somil16022006@gmail.com>
2025-11-09 05:35:15 +0530

    Fixed forgot password bug

--- src/main/java/org/spacehub/DTO/DTO_auth/ForgotPasswordRequest.java
+++ src/main/java/org/spacehub/DTO/DTO_auth/ForgotPasswordRequest.java
@@ -1,16 +1,13 @@
-
 package org.spacehub.DTO.DTO_auth;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Setter
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ForgotPasswordRequest {
   private String email;
-
-  public ForgotPasswordRequest(String email) {
-    this.email = email;
-  }
 
 }
