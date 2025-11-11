db11d116732586a642ed1de400dcf3f7b151a8be
somiljain2006 <somil16022006@gmail.com>
2025-11-11 22:45:04 +0530

    Dropping database

--- src/main/java/org/spacehub/repository/community/CommunityRepository.java
+++ src/main/java/org/spacehub/repository/community/CommunityRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
