29dfc9a9e6f8a01e3792c18bd3bc39b5f467f194
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-11-17 14:01:28 +0530

    Updated the security endpointds

--- src/main/java/org/spacehub/configuration/SecurityConfiguration.java
+++ src/main/java/org/spacehub/configuration/SecurityConfiguration.java
@@ -67,30 +67,29 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(auth -> auth
                             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                             .requestMatchers(
-                                    "/ws-messages/**",
-                                    "/api/v1/voice-room/**",
-                                    "/api/v1/**",
-                                    "/api/**",
-                                    "/ws/**",
-                                    "/swagger-ui/**",
-                                    "/v3/api-docs/**",
-                                    "/v3/api-docs.yaml",
-                                    "/chat",
-                                    "/chat/**",
-                                    "/files/**",
-                                    "/notification/**",
-                                    "/wss/**"
-//                      "/api/v1/login",
-//                      "/api/v1/registration",
-//                      "/api/v1/validateregisterotp",
-//                      "/api/v1/forgotpassword",
-//                      "/api/v1/validateforgototp",
-//                      "/api/v1/resetpassword",
-//                      "/api/v1/resendotp",
-//                      "/api/v1/resendforgototp",
-//                      "/swagger-ui/**",
-//                      "/v3/api-docs/**"
-
+//                                    "/ws-messages/**",
+//                                    "/api/v1/voice-room/**",
+//                                    "/api/v1/**",
+//                                    "/api/**",
+//                                    "/ws/**",
+//                                    "/swagger-ui/**",
+//                                    "/v3/api-docs/**",
+//                                    "/v3/api-docs.yaml",
+//                                    "/chat",
+//                                    "/chat/**",
+//                                    "/files/**",
+//                                    "/notification/**",
+//                                    "/wss/**"
+                      "/api/v1/login",
+                      "/api/v1/registration",
+                      "/api/v1/validateregisterotp",
+                      "/api/v1/forgotpassword",
+                      "/api/v1/validateforgototp",
+                      "/api/v1/resetpassword",
+                      "/api/v1/resendotp",
+                      "/api/v1/resendforgototp",
+                      "/swagger-ui/**",
+                      "/v3/api-docs/**"
                             ).permitAll()
                             .anyRequest().authenticated()
             )
